814ffb1c9551e823f5cffcdba06953f9a6285a82
Nikkakko <nikolozkopadze@gmail.com>
2022-07-28 13:21:22 +0400

    ahref changed

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -132,10 +132,7 @@ const CheckoutForm = () => {
         {/* Show a success message upon completion*/}
         <p className={succeeded ? 'result-message' : 'result-message hidden'}>
           Payment succeeded, see the result in your{' '}
-          <a
-            href={`https://dashboard.stripe.com/test/payments`}
-            target='_blank'
-          >
+          <a href={`https://dashboard.stripe.com/test/payments`}>
             Stripe dashboard.
           </a>
           Refresh the page to pay again
